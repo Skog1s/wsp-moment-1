75a21cfad889e9d125e61fd347ec6b6289815c08
Emil Skoglund~ <06skem@skola.engelholm.se>
2024-08-26 08:58:50 +0200

    first commit

---
+++ www/public/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<meta charset="UTF-8">
+
+<head>
+   <link rel="stylesheet" href="/css/style.css">
+   <script src="/js/script.js"></script>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <script>(function (a, b, c) { if (c in b && b[c]) { var d, e = a.location, f = /^(a|html)$/i; a.addEventListener("click", function (a) { d = a.target; while (!f.test(d.nodeName)) d = d.parentNode; "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href) }, !1) } })(document, window.navigator, "standalone")</script>
+
+
+</head>
+
+
+<body>
+
+   <form>
+      <label>Namn</label><br>
+      <input type="text">
+      <br>
+      <input type="submit" value="Skicka">
+      <br>
+      <label>Meddelande</label><br>
+      <textarea name="meddelande" cols="45" rows="5">
+    </textarea>
+      <br>
+      <input type="submit" value="Skicka">
+   </form>
+   <header>
+
+   </header>
+
+   <main>
+
+   </main>
+
+   <footer>
+
+   </footer>
+</body>
+
+</html>
